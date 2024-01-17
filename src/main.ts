87fffa8f9dc4e39dcbb82c63f680467381d1e513
DanT2000 <tretyakovdodant@gmail.com>
2024-01-17 05:37:08 +0300

    fix_Problems

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Door } from "./door";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Окружение
-const environmentTexture = new THREE.TextureLoader().load('background.jpg');
+const environmentTexture = new THREE.TextureLoader().load("background.jpg");
 scene.background = environmentTexture;
 
 // Освещение
@@ -18,7 +24,7 @@ mainLight.position.set(0, 20, 0);
 scene.add(mainLight);
 
 const sideLight = new THREE.DirectionalLight(0xffffff, 1);
-sideLight.position.set(5, 4, 14); // Направление бокового света
+sideLight.position.set(5, 4, 14); 
 scene.add(sideLight);
 
 // Настройка теней
@@ -35,35 +41,23 @@ const sphereGeometry = new THREE.SphereGeometry();
 const boxMaterial = new THREE.MeshStandardMaterial({
   color: 0x00ff00, // Зеленый цвет
   metalness: 0.5,
-  roughness: 0.5
+  roughness: 0.5,
 });
 
 const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: 0xff0000, // Красный цвет
-  metalness: 0.8,
-  roughness: 0.2
+  color: 0xff0000,
+  metalness: 1.0,
+  roughness: 0.3,
 });
 
-
-const doorTexture = new THREE.TextureLoader().load('door.png');
-doorTexture.wrapS = THREE.RepeatWrapping;
-doorTexture.wrapT = THREE.RepeatWrapping;
-const platformTexture = new THREE.TextureLoader().load('floor.png');
-const platformBumpMap = new THREE.TextureLoader().load('floor_bump.png');
-const doorMaterial = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  metalness: 0,
-  roughness: 0.5,
-  map: doorTexture,
-  bumpScale: 0.1 
-});
-
+const platformTexture = new THREE.TextureLoader().load("floor.png");
+const platformBumpMap = new THREE.TextureLoader().load("floor_bump.png");
 
 // Создание объектов
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-boxMesh.position.x = -2;
-sphereMesh.position.x = 2;
+boxMesh.position.x = -4;
+sphereMesh.position.x = 4;
 
 boxMesh.castShadow = true;
 sphereMesh.castShadow = true;
@@ -71,12 +65,12 @@ sphereMesh.castShadow = true;
 // Создание платформы
 const platformGeometry = new THREE.BoxGeometry(10, 0.1, 10);
 const platformMaterial = new THREE.MeshStandardMaterial({
-  color: 0xCCCCCC,
+  color: 0xcccccc,
   metalness: 0,
   roughness: 0.5,
   map: platformTexture,
   bumpMap: platformBumpMap,
-  bumpScale: 1 
+  bumpScale: 1,
 });
 const platform = new THREE.Mesh(platformGeometry, platformMaterial);
 platform.position.y = -2;
@@ -86,53 +80,55 @@ scene.add(platform);
 scene.add(boxMesh);
 scene.add(sphereMesh);
 
+const door = new Door();
+scene.add(door.getDoor());
 
-// Создание двери
-const doorGeometry = new THREE.BoxGeometry(1, 2, 0.1);
-const door = new THREE.Mesh(doorGeometry, doorMaterial);
-door.position.y = -1; // Высота половины двери
-door.castShadow = true;
-scene.add(door);
-
-// Камера
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Ползунки
-const doorWidthSlider: HTMLInputElement = document.createElement('input');
-const doorHeightSlider: HTMLInputElement = document.createElement('input');
+// GUI 
+const doorWidthSlider: HTMLInputElement = document.createElement("input");
+const doorHeightSlider: HTMLInputElement = document.createElement("input");
 
-doorWidthSlider.type = 'range';
-doorWidthSlider.min = '1';
-doorWidthSlider.max = '5';
-doorWidthSlider.value = '1';
+doorWidthSlider.type = "range";
+doorWidthSlider.min = "100";
+doorWidthSlider.max = "500";
+doorWidthSlider.value = "200";
 
+doorHeightSlider.type = "range";
+doorHeightSlider.min = "100";
+doorHeightSlider.max = "500";
+doorHeightSlider.value = "300";
 
-doorHeightSlider.type = 'range';
-doorHeightSlider.min = '1';
-doorHeightSlider.max = '10';
-doorHeightSlider.value = '1';
-
-
-doorWidthSlider.addEventListener('input', () => {
-  door.scale.x = parseFloat(doorWidthSlider.value);
-  doorTexture.repeat.set(parseFloat(doorWidthSlider.value),parseFloat(doorHeightSlider.value));
-});
-doorHeightSlider.addEventListener('input', () => {
-  door.scale.y = parseFloat(doorHeightSlider.value);
-  doorTexture.repeat.set(parseFloat(doorWidthSlider.value),parseFloat(doorHeightSlider.value));
+doorWidthSlider.addEventListener("input", () => {
+  const newWidth = parseFloat(doorWidthSlider.value) / 100;
+  const currentHeight = parseFloat(doorHeightSlider.value) / 100;
+  door.resizeDoor(newWidth, currentHeight);
 });
 
-// GUI ползунков
+doorHeightSlider.addEventListener("input", () => {
+  const currentWidth = parseFloat(doorWidthSlider.value) / 100;
+  const newHeight = parseFloat(doorHeightSlider.value) / 100;
+  door.resizeDoor(currentWidth, newHeight);
+});
+
+const widthLabel: HTMLDivElement = document.createElement('div');
+widthLabel.textContent = 'Ширина';
+widthLabel.className = 'label';
+const heightLabel: HTMLDivElement = document.createElement('div');
+heightLabel.textContent = 'Высота';
+heightLabel.className = 'label';
+
 const gui: HTMLDivElement = document.createElement('div');
 gui.id = 'gui';
+gui.appendChild(widthLabel);
 gui.appendChild(doorWidthSlider);
+gui.appendChild(heightLabel);
 gui.appendChild(doorHeightSlider);
 document.body.appendChild(gui);
 
-// Рендеринг
 function animate() {
   requestAnimationFrame(animate);
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     const newWidth = window.innerWidth;
     const newHeight = window.innerHeight;
 
@@ -142,8 +138,7 @@ function animate() {
     renderer.setSize(newWidth, newHeight);
   });
 
-  // Вращение двери
-  door.rotation.y += 0.01;
+  
 
   controls.update();
   renderer.render(scene, camera);
